8c6b6d3cf5cb3ba5c9318a3afbd10bae73197158
Darksied95 <aliyuefe9501@gmail.com>
2023-02-01 08:39:10 +0100

    clean up code

--- CleanUpNews.js
+++ CleanUpNews.js
@@ -2,9 +2,11 @@ const fs = require('fs/promises')
 
 
 module.exports = function () {
-    fs.readdir('./news').then(data => {
-        data.forEach(each => {
-            fs.unlink('./news/' + each,)
+    fs
+        .readdir('./news')
+        .then(data => {
+            data.forEach(each => {
+                fs.unlink('./news/' + each,)
+            })
         })
-    })
 }
--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 const puppeteer = require('puppeteer')
 const cleaner = require('./CleanUpNews')
 
+/*
 
+Cleans news folder
+
+*/
 cleaner()
+const section = 'crime'
+
 async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.nairaland.com/crime')
+    await page.goto(`https://www.nairaland.com/${section}`)
 
     const news = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".body tr td b a"), (e) => e.href)
+        return Array.from(document.querySelectorAll(".body tr td b a"), e => e.href)
     })
 
-    const filteredNews = news.filter(each => {
-        if (each.split('/').length > 4) return each
-    })
+    const filteredNews = news.filter(each => each.split('/').length > 4 ? each : false)
 
 
-    for (const each of filteredNews) {
-        await page.goto(each)
-        await page.screenshot({ path: './news/' + each.split('/').at(-1) + '.png', fullPage: true })
+    for (let i = 0; i < filteredNews.length; i++) {
+        await page.goto(filteredNews[i])
+        await page.screenshot({ path: './news/' + filteredNews[i].split('/').at(-1) + '.png', fullPage: true })
+        console.clear()
+
+            (i !== filteredNews.length - 1)
+            ?
+            console.log(Math.floor(i / filteredNews.length * 100) + '%')
+            :
+            console.log('Completed')
     }
-
-
-
     await browser.close()
 }
 
